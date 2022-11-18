6925d51f6da8b95c66284d02be4db1007f2c609b
MinseoKangQ <kms02171@naver.com>
2022-11-18 17:06:15 +0900

    p7

--- Chap09/p7.java
+++ Chap09/p7.java
@@ -10,7 +10,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class p7 extends JFrame {
+public class p7 extends JFrame { // JFrame 상속받기 
 
 	class NorthPanel extends JPanel {
 		
@@ -67,23 +67,26 @@ public class p7 extends JFrame {
 		}
 	}
 
+	// 생성자
 	public p7() {
 		
-		setTitle("계산기 프레임");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("계산기 프레임"); //타이틀
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 창 닫히면 프로그램 종료
 		
-		Container c = getContentPane();
-		c.setLayout(new BorderLayout());
+		Container c = getContentPane(); // 컨텐트팬 받기
+		c.setLayout(new BorderLayout()); // 배치관리자 설정
 		
-		c.add(new NorthPanel(), BorderLayout.NORTH);
+		// 패널들 등록하고 배치 
+		c.add(new NorthPanel(), BorderLayout.NORTH); 
 		c.add(new CenterPanel(), BorderLayout.CENTER);
 		c.add(new SouthPanel(), BorderLayout.SOUTH);
 		
-		setSize(500, 300);
-		setVisible(true);
+		setSize(500, 300); // 창 크기 설정 
+		setVisible(true); // 창 보이게
 		
 	}
 	
+	// main 함수
 	public static void main(String [] args) {
 		new p7();
 	}
